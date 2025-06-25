0c5983c70434ff10f7786232a080384903fa1b77
kukanishka7 <kukanishka7@gmail.com>
2025-06-25 21:22:52 +0530

    updating allowed hosts in vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     open: true,
+    allowedHosts: [
+      'forgemycode-production.up.railway.app',
+      'localhost',
+      'forgemycode.com'
+    ]
   },
 })
